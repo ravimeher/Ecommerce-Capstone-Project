c4044d55c79a54efe065a710ea9093be513bb016
Ravi Meher <vravimeher@gmail.com>
2024-08-19 17:13:40 +0530

    fetch modes

--- src/main/java/org/example/productcatalogservice/model/Category.java
+++ src/main/java/org/example/productcatalogservice/model/Category.java
@@ -6,6 +6,8 @@ import jakarta.persistence.FetchType;
 import jakarta.persistence.OneToMany;
 import lombok.Getter;
 import lombok.Setter;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.List;
@@ -18,5 +20,6 @@ public class Category extends BaseModel{
     private String name;
     private String description;
     @OneToMany(mappedBy = "category",fetch = FetchType.EAGER)
+    @Fetch(FetchMode.SELECT)
     private List<Product> productList;
 }
--- src/test/java/org/example/productcatalogservice/repository/CategoryRepoTest.java
+++ src/test/java/org/example/productcatalogservice/repository/CategoryRepoTest.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -25,6 +26,28 @@ class CategoryRepoTest {
         System.out.println(optionalCategory.get().getName());
         for(Product p : optionalCategory.get().getProductList()) {
             System.out.println(p.getName());
+        }
+    }
+
+    @Test
+    @Transactional
+    public void testFetchTypesAndModes() {
+        Optional<Category> optionalCategory = categoryRepo.findById(1);
+        System.out.println(optionalCategory.get().getName());
+        for(Product p : optionalCategory.get().getProductList()) {
+            System.out.println(p.getName());
+        }
+    }
+
+    @Test
+    @Transactional
+    public void testSomething() {
+        List<Category> categoryList = categoryRepo.findAll();
+        for(Category category : categoryList) {
+            List<Product> products = category.getProductList();
+            if(!products.isEmpty()) {
+                System.out.println(products.get(0).getName());
+            }
         }
     }
 }
