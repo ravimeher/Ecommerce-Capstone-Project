b60986f776372998a4ec381bac8382387e205451
Ravi Meher <vravimeher@gmail.com>
2024-11-02 21:30:34 +0530

    Service Discovery Integration

---
+++ src/main/java/org/example/userauthenticationservice/controllers/UserController.java
@@ -0,0 +1,29 @@
+package org.example.userauthenticationservice.controllers;
+
+import org.example.userauthenticationservice.dtos.UserDto;
+import org.example.userauthenticationservice.models.User;
+import org.example.userauthenticationservice.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+    @GetMapping("/users/{uid}")
+    public UserDto getUserById(@PathVariable Long uid){
+        User user = userService.getUser(uid);
+        return from(user);
+    }
+
+    private UserDto from (User user){
+        UserDto userDto = new UserDto();
+        userDto.setEmail(user.getEmail());
+        userDto.setRoleSet(user.getRoleSet());
+        return userDto;
+    }
+}
--- src/main/java/org/example/userauthenticationservice/models/Role.java
+++ src/main/java/org/example/userauthenticationservice/models/Role.java
@@ -1,8 +1,11 @@
 package org.example.userauthenticationservice.models;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
 import lombok.Setter;
+
+import java.util.Set;
 
 @Getter
 @Setter
