eee104a6c0ebbd444b51a5de615ba1518e5c1393
Ravi Meher <vravimeher@gmail.com>
2025-06-26 23:41:57 +0530

    Clean tracked IDE/build files and apply .gitignore

--- out/production/Backend_Capstone_Project/src/main/resources/db/migration/update-schema.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE test_modeldbmigration
-    ADD num_field INT NULL;
-
-ALTER TABLE test_modeldbmigration
-    ADD text_field VARCHAR(255) NULL;
--- out/production/Backend_Capstone_Project/src/main/resources/db/migration/update-schema2.sql
+++
@@ -1,2 +0,0 @@
-ALTER TABLE test_modeldbmigration
-    DROP COLUMN num_field;
